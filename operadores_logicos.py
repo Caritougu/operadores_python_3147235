91cb10e16a869448baa5a26d479d8813aab2b28e
Angel <angeldrc1612@gmail.com>
2025-03-07 08:58:55 -0500

    Operadores Booleanos

---
+++ operadores_logicos.py
@@ -0,0 +1,35 @@
+"""
+Los operadores logicos son:
+ 
+and,or,not
+
+
+obedencen las tablas de verdad:
+p/q/p and q/
+
+JERARQUIA DEFINITIVA DE OPERADORES
+1.         ()
+2.         **
+3.      *, / , % 
+4.        +, -
+5.  ,<, >=, <=, !=, ==
+6.          NOT
+7.          AND
+8.          OR
+9.          =
+
+
+
+"""
+
+op1= False
+op2= True 
+op3= op1 or op2
+
+print(op3)
+
+#operador not 
+
+op4 = not op2
+
+print(op4) 
